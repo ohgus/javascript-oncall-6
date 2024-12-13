c94a9b639d058e2077aa8554a1d1a647ce9d85ad
ohgus <123801385+ohgus@users.noreply.github.com>
2024-12-13 15:25:56 +0900

    feat: 근무 순번 입력 관련 로직 controller에 연결

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -3,10 +3,17 @@ import InputView from '../view/InputView.js';
 import { validateDay } from '../util/validator/day.js';
 import { validateMonth } from '../util/validator/month.js';
 import { tryInput } from '../util/tryInput.js';
+import {
+  validateWeekday,
+  validateWeekend,
+} from '../util/validator/workOrder.js';
 
 class Controller {
   async play() {
     const [month, day] = await tryInput(() => this.#readMonthDay());
+    const [weekdayOrder, weekendOrder] = await tryInput(() =>
+      this.#readOrders(),
+    );
   }
 
   async #readMonthDay() {
@@ -17,6 +24,18 @@ class Controller {
     validateDay(day);
 
     return [Number(month), day];
+  }
+
+  async #readOrders() {
+    const weekdayOrderInput = await InputView.readWeekday();
+    const weekdayOrder = weekdayOrderInput.split(',');
+    validateWeekday(weekdayOrder);
+
+    const weekendOrderInput = await InputView.readWeekend();
+    const weekendOrder = weekendOrderInput.split(',');
+    validateWeekend(weekendOrder, weekdayOrder);
+
+    return [weekdayOrder, weekendOrder];
   }
 }
 
